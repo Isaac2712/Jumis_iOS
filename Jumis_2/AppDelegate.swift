fe0b4520adee2c4b2a1e3f2b79e2704b49578c91
Isaac Marroqui <isaac.marroc@gmail.com>
2022-12-22 12:00:06 +0100

    22.12.2022 11:59 BBDD ok

--- Jumis_2/AppDelegate.swift
+++ Jumis_2/AppDelegate.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
+var databasePointer: OpaquePointer?
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if let dbPointer = DBHelper.getDatabasePointer(databaseName: "BDJumis.sqbpro") {
+            databasePointer = dbPointer
+        } else {
+             
+        }
+        
         return true
     }
 
---
+++ Jumis_2/DataBase/DBHelper.swift
@@ -0,0 +1,44 @@
+//
+//  DBHelper.swift
+//  Jumis
+//
+//  Created by Isaac Marroquí Penalva on 22/12/22.
+//
+
+import Foundation
+import SQLite3
+
+class DBHelper{
+    static func getDatabasePointer(databaseName: String) -> OpaquePointer? {
+        var databasePointer: OpaquePointer?
+        
+        let documentDatabasePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(databaseName).path
+        
+        if FileManager.default.fileExists(atPath: documentDatabasePath){
+            print("Database exists (alredy")
+        } else {
+            guard let bundleDatabasePath = Bundle.main.resourceURL?.appendingPathComponent(databaseName).path else {
+                print("Error: Database Path doesn´t exist")
+                return nil
+            }
+            
+            do {
+                try FileManager.default.copyItem(atPath: bundleDatabasePath, toPath: documentDatabasePath)
+                print("Database created (copied)")
+            } catch {
+                    print("Error: \(error.localizedDescription)")
+                    return nil
+            }
+        }
+        
+        if sqlite3_open(documentDatabasePath, &databasePointer) == SQLITE_OK {
+            print("Succesfully open database")
+            print("Database path: \(documentDatabasePath)")
+        } else {
+            print("Could not open database")
+        }
+        
+        return databasePointer
+    }
+}
+
